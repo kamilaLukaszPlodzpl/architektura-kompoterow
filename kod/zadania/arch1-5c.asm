90ce244554cd24aa5501f092a3a0f0fc4e69b942
lukassz111 <lukassz111@gmail.com>
2019-03-22 19:11:26 +0100

    com

--- kod/zadania/arch1-5c.asm
+++ kod/zadania/arch1-5c.asm
@@ -5,26 +5,29 @@
 ; Cwiczenie      : Kompilacja, konsolidacja i debugowanie programow           ;
 ;                  asemblerowych                                              ;
 ; Autorzy        : Kamila Kossyn, Łukasz Narloch, grupa 1, czw., 16:00-17:30  ;
-; Data zaliczenia: DD.03.2019                                                 ;
+; Data zaliczenia: 28.03.2019                                                 ;
 ; Uwagi          : Program obliczajacy wzor: (a*b+c*d)/(a-d)                  ;
 ;                                                                             ;
 ;=============================================================================;
 
-                .MODEL  TINY
+                .MODEL  TINY;Model mówiący o tym że kod bedzie składał się z jednego segmentu
 
 Kod             SEGMENT
 
+                ;Program w formacie COM wymaga przesunięcia o 256 bajtów
                 ORG     100h
                 ASSUME  CS: Kod, DS: Kod, SS:Kod
 
 Start:
+                ;Wymagany skok do reszty rozkazów ze względu na to że
+                ;trzeba pominąć obszar pamięci zawierający dane a nie rozkazy
                 je      Poczotek
 
-a               DB      20
-b               EQU     10
-c               DW      5
+a               DB      20;Bajt z liczba 20
+b               EQU     10;Stała
+c               DW      5;Word 2 bajty z liczba 5
 d               =       3
-Wynik           DB      ?
+Wynik           DB      ?;Bajt
 
 Poczotek:
                 ;Mnożenie a*b wynik do ax
@@ -32,31 +35,37 @@ Poczotek:
                 mov ah, b
                 mul ah
 
+                ;Obliczone a*b przesuwamy do bx by zwolnić ax
+                ;którego porzebujemy dalej w naszym programie
                 mov     bx, ax
+
                 ;Mnożenie c*d wynik do ax
                 mov     ax, d
                 mov     cx, ax
                 mov     ax, c
                 mul     cx
                 
-                ;Dodanie a*b+c*d
+                ;Dodanie a*b+c*d wynik do bx
                 add     bx, ax
 
                 ;Odjęcie a-d wynik do ax
-                xor     ax, ax
+                xor     ax, ax;Zerowanie ax
                 mov     al, a
                 sub     al, d
 
-                mov     cx, ax;Dzielinik
-                mov     ax, bx
+                ;Dzielenie 2 liczb wynik do ax
+                mov     cx, ax;
+                mov     ax, bx;
                 div     cx
 
-                mov     al, Wynik
 
-                mov     ax, 4C25h
-                int     21h
+                mov     al, Wynik;Ustawiamy exit code na nasz wynik
+
+                mov     ah, 4Ch;Ustawia wywolanie systemowe na wywolanie zwaracajace kontrole procesowi rodzicowi
+                int     21h;Przerwanie 21h, uruchamia wywolanie systemowe
 
 Kod            ENDS
-
+                
+                
                 END Start
 
