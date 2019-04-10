4895c8e9d0f53911dc66b79e4ed59e56d53eae62
lukassz111 <lukassz111@gmail.com>
2019-04-11 00:17:10 +0200

    kod

--- kod/test/proc.asm
+++ kod/test/proc.asm
@@ -4,6 +4,7 @@
 Data SEGMENT USE16
 
 aTxt    DW 0
+toNieJestCyfraNapis DB "To nie jest cyfra!$"
 
 ENDS
 
@@ -14,27 +15,87 @@ Start:
     mov ds, ax
 
     mov di, OFFSET aTxt
-    call ReadNumer
-
+    mov [di], 3039h
+    
+EndProg:
     mov ax, 4C00h
     int 21h
 
-    ReadNumer PROC; Wczytuje liczbe do pamięci pod adresem w di
-        call ReadChar
+    ReadNumer PROC
+        ;Wczytuje liczbe do pamięci pod adresem w di
+        ;di - adres na Word
+        ;dl = 1 liczba ujemna
+        ;dl = 0 liczba dodatnia
+        ;al - wczytana litera
+        mov WORD PTR [di], 0
+        mov ah, 01h
+        int 21h
         cmp al, '-'
         je ReadNumer_PierwszyMinusWczytano
         cmp al, '+'
         je ReadNumer_PierwszyPlusWczytano
         cmp al, '0'
-        jl ReadNumer_PierwszyInneWczytano
+        jl ReadNumer_InneWczytano
         cmp al, '9'
-        jg ReadNumer_PierwszyInneWczytano
+        jg ReadNumer_InneWczytano
         jmp ReadNumer_PierwszyCyfreWczytano
     ReadNumer_PierwszyMinusWczytano:
-        mov WORD PTR di, 8000h;Ustawia znak na ujemny
+        mov dl, 1;Zapisuje info o znaku
+        mov cx, 5;Pozostalo do wczytania 5 cyfr
+        jmp ReadNumer_ReadDigit
     ReadNumer_PierwszyPlusWczytano:
+        mov dl, 0;Zapisuje info o znaku
+        mov cx, 5;
+        jmp ReadNumer_ReadDigit
     ReadNumer_PierwszyCyfreWczytano:
-    ReadNumer_PierwszyInneWczytano:
+        mov dl, 0;Zapisuje info o znaku
+        mov cx, 4
+        ;bl - wczytana cyfra
+        ;Kod ascii lirery - '0' jako kod ascii
+        mov bl, al
+        mov bh, '0'
+        sub bl, bh
+        xor bh, bh
+        ;Dopisanie cyfry
+        mov dl, '+'
+        call PrintChar
+        
+        add WORD PTR [di],bx
+        jmp ReadNumer_ReadDigit
+    ReadNumer_InneWczytano:
+        mov ah, 09h
+        mov dx, OFFSET toNieJestCyfraNapis
+        int 21h
+        mov ax, 4C00h
+        int 21h
+
+    ReadNumer_ReadDigit:
+        ;ax - zmienia sie
+        ;Przesunięcie na kolejne miejsce
+        
+        mov dl, '*'
+        call PrintChar
+        mov ax,[di]
+        mov bl, 10
+        mul bl
+        mov [di], ax
+        ;Wczytanie kolejnej cyfry
+        mov ah, 01h
+        int 21h
+        cmp al, '0'
+        jl ReadNumer_InneWczytano
+        cmp al, '9'
+        jg ReadNumer_InneWczytano
+        mov bl, '0'
+        sub al, bl
+        xor ah, ah
+        mov dl, '+'
+        call PrintChar
+        add WORD PTR [di], ax
+        loop ReadNumer_ReadDigit
+
+
+    ReadNumer_end:
         ret
     ENDP
     
