de209358ae60d4b5cf4aac89578ede8137ff261c
lukassz111 <lukassz111@gmail.com>
2019-05-27 20:46:07 +0200

    4 częściowo

--- kod/zadania/zad4/main.cpp
+++ kod/zadania/zad4/main.cpp
@@ -9,34 +9,116 @@
 //;                                                                             ;
 //;=============================================================================;
 
-#include <dos.h>
+#define SCREEN_WIDTH 320
+#define SCREEN_HEIGHT 200
 
-int main()
+#include <dos.h>
+#include <stdio.h>
+#include <iostream.h>
+
+typedef unsigned char BYTE;
+typedef unsigned int WORD;
+typedef unsigned int UINT;
+typedef unsigned long DWORD;
+typedef unsigned long LONG;
+
+unsigned char far* video_memory = (unsigned char far*) 0xA0000000L;
+
+void setPixel(int x, int y, BYTE color)
+{
+    int addr = ((SCREEN_WIDTH*SCREEN_HEIGHT) - (SCREEN_WIDTH*y)) + x;
+    video_memory[addr] = color;
+}
+BYTE getPixel(int x, int y)
+{
+    int addr = ((SCREEN_WIDTH*SCREEN_HEIGHT) - (SCREEN_WIDTH*y)) + x;
+    return video_memory[addr];
+}
+void graphicsMode()
 {
     REGPACK regs; 
     regs.r_ax = 0x13;
     intr(0x10, &regs);
-    
-    unsigned char *video_memory = (byte *)0xA0000000L;
-    //ustaw lewy górny piksel obrazu na kolor 128
-    //aktualnej palety karty VGA 
-    video_memory[0] = 128;
-    //ustaw prawy dolny piksel obrazu na kolor 255
-    //aktualnej palety karty VGA 
-    video_memory[64000] = 255;
+}
+void textMode()
+{
+    REGPACK regs; 
+    regs.r_ax = 0x10;
+    intr(0x10, &regs);
+}
+void setColorsPallete()
+{
+    outportb(0x03C8, 0); //rozpocznij ustawianie palety od koloru nr 0
+	for (int i = 0; i < 256; i++)
+	{
+		outp(0x03C9,i * 63 / 255);//R
+		outp(0x03C9,i * 63 / 255);//G
+		outp(0x03C9,i * 63 / 255);//B
+	}
+}
 
-    outportb(0x03C8, 0);
-    //rozpocznij ustawianie palety od koloru nr 0
-    for (int i = 0; i < 255; i++)
-    //ilość kolorów w palecie 8-bitowej
+struct BITMAPFILEHEADER
+{
+    UINT  bfType;//Opis formatu pliku. Musi być ‘BM’.
+    DWORD bfSize;//Rozmiar pliku BMP w bajtach.
+    UINT  bfReserved1;//Zarezerwowane. Musi być równe 0.
+    UINT  bfReserved2;//Zarezerwowane. Musi być równe 0.
+    DWORD bfOffBits;//Przesunięcie w bajtach początku danych
+    //obrazu liczone od końca struktury 
+};
+struct BITMAPINFOHEADER
+{
+    DWORD biSize;//Rozmiar struktury BITMAPINFOHEADER.
+    LONG  biWidth;//Szerokość bitmapy w pikselach.
+    LONG  biHeight;//Wysokość bitmapy w pikselach.
+    WORD  biPlanes;//Ilość płaszczyzn. Musi być 1.
+    WORD  biBitCount;//Głębia kolorów w bitach na piksel.
+    DWORD biCompression;//Rodzaj kompresji (0 – brak).
+    DWORD biSizeImage;//Rozmiar obrazu w bajtach. Uwaga może być 0.
+    LONG  biXPelsPerMeter;//Rozdzielczość pozioma w pikselach na metr.
+    LONG  biYPelsPerMeter;//Rozdzielczość pionowa w pikselach na metr.
+    DWORD biClrUsed;//Ilość używanych kolorów z palety.
+    DWORD biClrImportant;//Ilość kolorów z palety niezbędnych do
+    //wyświetlenia obrazu
+};
+struct RGBQUAD//Kolor palety w RGB
+{
+    BYTE rgbBlue;
+    BYTE rgbGreen;
+    BYTE rgbRed;
+    BYTE rgbReserved;
+};
+int printBitmap(const char* fileName)
+{
+    BITMAPFILEHEADER bmfh;
+    BITMAPINFOHEADER bmih;
+    FILE *bmpFile = fopen(fileName, "rb");
+    if (bmpFile == NULL)
     {
-        outp(0x03C9, palette[i].rgbRed   * 63 / 255);
-        //skalowana składowa R
-        outp(0x03C9, palette[i].rgbGreen * 63 / 255);
-        //skalowana składowa
-        outp(0x03C9, palette[i].rgbBlue  * 63 / 255);
-        //skalowana składowa B
+        cout << "Nie udalo sie otworzyc pliku: " << fileName << " !\n";
+        return 1;
     }
+    fread(&bmfh, sizeof(BITMAPFILEHEADER), 1 , bmpFile);
+    fread(&bmih, sizeof(BITMAPINFOHEADER), 1 , bmpFile);
 
+    
+    for(WORD i = 0; i <= 64000; i++)
+    {
+        BYTE color = fgetc(bmpFile);
+        video_memory[63999-i] = color;
+    }
+    fclose(bmpFile);
     return 0;
+}
+int main()
+{
+    graphicsMode();
+    setColorsPallete();
+    char fileName[] = "boat.bmp";
+    printBitmap(fileName);
+
+    cin.get();
+    cin.get();
+    textMode();
+   return 0;
 }
